82880bba2915e66fdaa89f8bc998678656985990
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2019-05-20 21:19:18 +0200

    Add files via upload

---
+++ EMPractica/src/main/resources/static/src/objects/interface.js
@@ -0,0 +1,11 @@
+//Recibe como parámetro un texto  
+//Modifica su color al pasar el ratón por encima
+function mouseOver(text) {
+    text.fill = "rgb(255,0,255)";
+};
+
+//Recibe como parámetro un texto  
+//Modifica su color al apartar el ratón
+function mouseOut(text) {
+    text.fill = "rgb(255,255,255)";
+};
