9b012f9f0413ab1581d2e8ef9d124dc1aa83e63d
Dani020998 <danim0209@hotmail.com>
2019-05-20 19:39:57 +0200

    lobbby buttons

--- EMPractica/src/main/resources/static/src/states/lobby.js
+++ EMPractica/src/main/resources/static/src/states/lobby.js
@@ -15,10 +15,53 @@ Spacewar.lobbyState.prototype = {
 	},
 
 	create : function() {
-		game.state.start('matchmakingState')
+		var style = {
+			fill : "rgb(255,255,255)",
+			font : "60px Chakra Petch",
+			boundsAlignH : "center"
+		};
+
+		lobbyOptions=["Crear Partida", "Buscar Partida"];
+        var y=game.canvas.height/3;
+        var yOffset= 80;
+        var lobbyText=[];
+        for(var i= 0; i<lobbyOptions.length; i++)
+            {
+               lobbyText[i] = game.add.text(0, y, lobbyOptions[i], style);
+               lobbyText[i].setTextBounds(0,0,game.world.width,game.world.height);
+               // Añade detección de eventos en cada texto
+               lobbyText[i].inputEnabled = true;
+               lobbyText[i].events.onInputOver.add(this.over,this);
+               lobbyText[i].events.onInputOut.add(this.out,this);
+                // Indica a qué función llamar dependiendo de qué texto se trate
+                switch(i){
+                    case 0:
+                        //menuText[i].events.onInputDown.add(this.play,this);
+                        break;
+                    case 1:
+                        // menuText[i].events.onInputDown.add(this.options,this);
+                        break;
+                }
+
+                y+= yOffset;
+            }
+		//game.state.start('matchmakingState')
 	},
 
 	update : function() {
+		
+	},
 
+	//Recibe como parámetro un texto  
+	//Modifica su color al pasar el ratón por encima, reproduciendo un sonido
+	over : function(text) {
+
+	    text.fill = "rgb(255,0,255)";
+	},
+	
+	//Recibe como parámetro un texto  
+	//Modifica su color al apartar el ratón
+	out : function(text) {
+	    text.fill = "rgb(255,255,255)";
 	}
 }
