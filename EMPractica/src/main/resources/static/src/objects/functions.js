7483d65a5f7b1d8a20ca79346d2de617e7aca652
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2019-05-22 19:14:33 +0200

    Funciones adaptadas a inputText

--- EMPractica/src/main/resources/static/src/objects/functions.js
+++ EMPractica/src/main/resources/static/src/objects/functions.js
@@ -1,8 +1,16 @@
-function inputAnotherRoomName(){
-	let name = window.prompt("Introduzca su nombre de sala: ");
+function setRoomName(name) {
 	let message = {
 		event : 'CREATE ROOM',
-		sala: name
+		sala : name
+	}
+	game.global.socket.send(JSON.stringify(message))
+}
+
+function setPlayerName(name) {
+	game.global.myPlayer.name = name;
+	let message = {
+		event : 'PLAYER NAME',
+		playerName : game.global.myPlayer.name
 	}
 	game.global.socket.send(JSON.stringify(message))
 }
--- EMPractica/src/main/resources/static/src/objects/interface.js
+++ EMPractica/src/main/resources/static/src/objects/interface.js
@@ -10,23 +10,27 @@ function mouseOut(text) {
 	text.fill = "rgb(255,255,255)";
 };
 
-
 // Crea un recuadro en el que introducir texto
-//Recibe como parámetros el color de el recuadro, el color del texto, el texto que aparece encima de la barra y el texto del botón
+// Recibe como parámetros el color de el recuadro, el color del texto, el texto
+// que aparece encima de la barra y el texto del botón
 function inputText(color, colorText, pText, sText) {
-	
+
 	this.iDiv = document.getElementById("textBlock");
-	this.iDiv.style.visibility="visible"
+	this.iDiv.style.visibility = "visible"
 	this.iDiv.style.backgroundColor = color;
-	
+
 	this.text = document.getElementById("p1");
 	this.text.style.color = colorText;
 	this.text.innerHTML = pText;
-	
-	this.input = document.getElementById("inputText");	
-	
+
+	this.input = document.getElementById("inputText");
+
 	this.submitButton = document.getElementById("submitButton");
 	this.submitButton.value = sText;
 
+	this.hide = function() {
+		this.input.value = "";
+		this.iDiv.style.visibility = "hidden";
+	}
 };
 
