987032393580493980257da25ebdf14bc1634096
MRxRafi <rafadriveclase@gmail.com>
2019-05-25 22:04:48 +0200

    Test y concurrencia

--- EMPractica/src/main/java/spacewar/WebsocketGameHandler.java
+++ EMPractica/src/main/java/spacewar/WebsocketGameHandler.java
@@ -81,18 +81,17 @@ public class WebsocketGameHandler extends TextWebSocketHandler {
 				player.getSession().sendMessage(new TextMessage(msg.toString()));
 				break;
 				
-			case "JOIN LOBBY":
-				
-				break;
 			case "CREATE ROOM":
-				boolean aux=game.waitRooms.containsKey(node.path("sala").asText());
-				if(!aux) {
-					game.waitRooms.put(node.path("sala").asText(), new WaitRoom(node.path("sala").asText(),player));
-					aux = true;
-				} else {
-					aux = false;
+				boolean aux = true;
+				synchronized(game) {
+					aux=game.waitRooms.containsKey(node.path("sala").asText());
+					if(!aux) {
+						game.waitRooms.put(node.path("sala").asText(), new WaitRoom(node.path("sala").asText(),player));
+						aux = true;
+					} else {
+						aux = false;
+					}
 				}
-
 				msg.put("event","CREATE ROOM");
 				msg.put("valido", aux);
 				msg.put("sala", node.path("sala").asText());
--- EMPractica/src/test/java/spacewar/SpacewarGameTest.java
+++ EMPractica/src/test/java/spacewar/SpacewarGameTest.java
@@ -2,7 +2,20 @@ package spacewar;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,23 +36,57 @@ public class SpacewarGameTest {
 	}
 
 	@Test
-	public void testJoin() throws Exception {
-
-		AtomicReference<String> firstMsg = new AtomicReference<String>();
-
+	public void testJoinThreads() throws Exception {
+		final int NUM_CLIENTES = 4;
+		
+		//Semaphore sem = new Semaphore(0);
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_CLIENTES);
+		CompletionService<String> completionService = new ExecutorCompletionService<>(executor);
+		
+		for(int i = 0; i < NUM_CLIENTES; i++) {
+			completionService.submit(()->testJoin());
+		}
+		
+		executor.shutdown();
+		executor.awaitTermination(200, TimeUnit.MILLISECONDS);
+		
+		for(int i = 0; i < NUM_CLIENTES; i++) {
+			try {
+				String msg = completionService.take().get();
+				assertTrue("The fist message should contain 'join', but it is " + msg, msg.contains("JOIN"));
+			} catch(ExecutionException e) {
+				Throwable cause = e.getCause();
+				System.out.println("Algo ha salido mal en " + cause.getMessage());
+			}
+			
+		}
+		
+	}
+	
+	public String testJoin() throws Exception{
 		WebSocketClient ws = new WebSocketClient();
+		AtomicReference<String> message = new AtomicReference<String>();
+		Semaphore sem = new Semaphore(0);
+		
+		/* El lock es necesario para que siga la ejecucion del codigo en el menor tiempo posible*/
 		
 		ws.onMessage((session, msg) -> {
 			System.out.println("TestMessage: " + msg);
-			firstMsg.compareAndSet(null, msg);
+			message.compareAndSet(null, msg);
+			sem.release();
 		});
 
 		ws.connect("ws://127.0.0.1:9000/spacewar");
+		sem.acquire();
 		System.out.println("Connected");
-		Thread.sleep(1000);
-		String msg = firstMsg.get();
-
-		assertTrue("The fist message should contain 'join', but it is " + msg, msg.contains("JOIN"));
+		
+		String jsonMessage = "{\"event\":\"CREATE ROOM\",\"sala\":\"sape\"}";
+		ws.sendMessage(jsonMessage);
+		System.out.println("Sending message CREATE ROOM..");
+		sem.acquire();
+		
+		
 		ws.disconnect();
+		return message.get();
 	}
 }
