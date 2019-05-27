f2ebf032e3be7fa12eeda56511d2bc795202710f
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2019-05-27 16:03:28 +0200

    Update menu.js

--- EMPractica/src/main/resources/static/src/states/menu.js
+++ EMPractica/src/main/resources/static/src/states/menu.js
@@ -61,7 +61,7 @@ Spacewar.menuState.prototype = {
 		titleText.setTextBounds(0, 0, game.world.width, game.world.height);
 
 		// Crea el texto de las opciones del menú
-		menuOptions = [ "Jugar", "Opciones" ];
+		menuOptions = [ "Jugar", "Cerrar Sesión" ];
 		var y = game.canvas.height / 3;
 		var yOffset = 80;
 		var menuText = [];
